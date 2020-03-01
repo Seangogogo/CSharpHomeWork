44242a9ef27d975805cb2d865785f04f0a8f8bcb
Seangogogo <52170235+Seangogogo@users.noreply.github.com>
2020-03-01 20:40:31 +0800

    Add files via upload

---
+++ HomeWork1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Window
+{
+    public partial class Form1 : Form
+    {
+        double firstNumber;
+        double secondNumber;
+        string myOperator;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                firstNumber = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            {
+                label4.Text = "your input is not a number";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                secondNumber = Convert.ToDouble(textBox2.Text);
+            }
+            catch
+            {
+                label4.Text = "your input is not a number";
+            }
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            myOperator = textBox3.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (myOperator)
+            {
+                case "+":
+                    label4.Text = $"{firstNumber + secondNumber}";
+                    break;
+                case "-":
+                    label4.Text = $"{firstNumber - secondNumber}";
+                    break;
+                case "*":
+                    label4.Text = $"{firstNumber * secondNumber}";
+                    break;
+                case "/":
+                    if (secondNumber < 1e-5)
+                        label4.Text = "can't devide 0";
+                    else
+                        label4.Text = $"{firstNumber / secondNumber}";
+                    break;
+                default:
+                    label4.Text = "your operator is invalid";
+                    break;
+            } 
+        }
+    }
+}
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main (string[] args)
+
+        {
+            bool endApp = false;
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
+
+            //获得运算的数字及运算符
+            Console.WriteLine("请输入数字A");
+            double A = Convert.ToDouble(Console.ReadLine());
+           
+            Console.WriteLine("请输入运算符");
+            string operate = Console.ReadLine();
+            Console.WriteLine("请输入数字B");
+            double B = Convert.ToDouble(Console.ReadLine());
+            //将结果存储到变量D
+            double D = 0;
+            // Use a if statement to do the math.
+            if (operate == "+")
+            {
+                D = A + B;
+                Console.WriteLine("A+B=" + D);
+
+            }
+            else if (operate == "-")
+            {
+                D = A - B;
+                Console.WriteLine("A-B=" + D);
+
+            }
+            else if (operate == "*")
+            {
+                D = A * B;
+                Console.WriteLine("A*B=" + D);
+            }
+            else if (operate == "/")
+            {
+                D = A / B;
+                // when it is a non-zero divisor.
+                if (B == 0)
+                    Console.WriteLine("不能除零操作！");
+                else
+                    Console.WriteLine("A/B=" + D);
+            }
+            else if (operate == "")
+            {
+                Console.WriteLine("The operator is null");
+            }
+            else
+            {
+                Console.WriteLine("The operator is illegal");
+            }
+
+                   
+         }
+            
+     }
+ }
+
+  
